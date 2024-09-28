d1adb87fc45a703b9ca98fdb57e6908a8cf8ecac
Oladejiraji <rajioladeji2@gmail.com>
2024-09-28 09:19:07 +0100

    chore/setup for shawen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { AppRoutes } from "@/utils/routes";
 import { redirect } from "next/navigation";
 
+// Landing page can be here
 export default async function Index() {
   return redirect(AppRoutes.dashboard.home.create.path);
 }
