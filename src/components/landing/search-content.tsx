0953e86ba898979e9a11bc3c1a7f12fe9db09718
Seun Oyediran <seunoyediran22517@gmail.com>
2024-10-01 09:03:00 +0100

    menu animation fix

--- src/components/landing/search-content.tsx
+++ src/components/landing/search-content.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 export default function SearchContent() {
   return (
-    <div className="app_search_content h-full px-7 pb-3 pt-4">
-      <input
-        className="app_search_content__input"
-        placeholder="Search anything"
-        autoFocus
-      />
+    <div className="app_menu_content flex h-full flex-col justify-between px-6 pt-2">
+      <div className="app_menu_content__ctt flex flex-1 flex-col gap-10">
+        {/* <h5 className="app_menu_content__ctt__title">
+          Umi is sill in beta and is constantly being updated
+        </h5> */}
+        <input
+          placeholder="Search anything"
+          className="app_search_content__input flex-1"
+        />
+      </div>
     </div>
   );
 }
